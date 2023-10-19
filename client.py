47360d495a4552bd6f0bf2af651a844f47c3b1c0
pauloarf <pauloferreira.vnc@gmail.com>
2023-10-19 09:27:11 +0100

    feat(project_structure): criados ficheiros de teste para sockets
    #criado ficheiro client e server para teste de sockets

---
+++ client.py
@@ -0,0 +1,27 @@
+import socket
+
+HEADERSIZE = 15
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))   #Normalmente nao é usado socket.gethostname(),temos de conectar a um public ip
+
+while True:
+    
+    full_msg = ''
+    new_msg = True
+    while True:
+        msg = s.recv(20) #Utilizamos 20 para receber mais um bocado do que o header
+        if new_msg:
+            print(f"new message lenght: {msg[:HEADERSIZE]}")
+            msglen = int(msg[:HEADERSIZE]) #Noutra linguagem, seria preciso retirar os espacos em "branco" deixados pelo header
+            new_msg = False
+
+        full_msg += msg.decode("utf-8")
+
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("full msg recvd")
+            print(full_msg[HEADERSIZE:])
+            new_msg = True
+            full_msg = ''
+    
+    print(full_msg)
